8c2eac180d6030f8cc43df03e14df35a89f3b24d
Carl Heyneman III <cheynemaniii@outlook.com>
2025-01-22 10:37:34 -0500

    Actually Fixed the Internal Settings with proper site nodes.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace LPS_Utility.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace LPS_Utility.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://www.kitco.com/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://www.kitco.com/charts/gold")]
         public string SilverWebsite {
             get {
                 return ((string)(this["SilverWebsite"]));
@@ -37,7 +37,7 @@ namespace LPS_Utility.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("//*[@id=\"__next\"]/main/div/div[1]/div[3]/div/div[2]/div[1]/div/span[1]")]
+        [global::System.Configuration.DefaultSettingValueAttribute("//*[@id=\"__next\"]/div/main/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/h3")]
         public string SilverPriceNode {
             get {
                 return ((string)(this["SilverPriceNode"]));
@@ -49,7 +49,7 @@ namespace LPS_Utility.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://www.kitco.com/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://www.kitco.com/charts/silver")]
         public string GoldWebsite {
             get {
                 return ((string)(this["GoldWebsite"]));
@@ -61,7 +61,7 @@ namespace LPS_Utility.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("//*[@id=\"__next\"]/main/div/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/span[1]")]
+        [global::System.Configuration.DefaultSettingValueAttribute("//*[@id=\"__next\"]/div/main/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/h3")]
         public string GoldPriceNode {
             get {
                 return ((string)(this["GoldPriceNode"]));
